283f4a627d74508a1a48addfae670fea03f35a77
Nikita Zdanovitch <nzdanovitch@gmail.com>
2018-11-30 23:18:33 +0300

    Move graphics interface here.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+extern crate nalgebra;
+
 pub struct Game<Bk, Scn> 
 where
     Bk: backend::Backend<Scn>,
@@ -183,3 +185,34 @@ pub mod dummy_backend {
         }
     }
 }
+
+pub mod graphics {
+    use nalgebra::*;
+
+    pub trait Color: Clone + Copy {
+        fn into_rgba(self) -> [f32; 4];
+    }
+
+    pub enum DrawMode {
+        Line(f32),
+        Fill,
+    }
+
+    pub trait Graphics {
+        fn clear<C: Color>(&mut self, C);
+        fn present(&mut self);
+        // fn draw(Drawable, position: Point2, scale: f32);
+    }
+
+    pub trait DrawPrimitives: Graphics {
+        fn set_color<C: Color>(&mut self, C);
+        fn circle(&mut self, DrawMode, origin: Point2<f32>, radius: f32);
+        fn ellipse(&mut self, DrawMode, origin: Point2<f32>, width: f32, height: f32);
+        fn line(&mut self, origin: Point2<f32>, target: Point2<f32>, width: f32);
+    }
+
+    pub trait Draw<S> {
+        fn draw(&self, &mut S);
+    }
+}
+
